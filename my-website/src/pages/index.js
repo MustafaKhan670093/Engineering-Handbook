13934bdc0320f423f8a6929158c64118e4ea1ea8
Mustafa Khan <48969731+MustafaKhan670093@users.noreply.github.com>
2021-04-28 22:56:28 -0400

    "second commit"

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Go To Engineering Handbook
           </Link>
         </div>
       </div>
